40a3418c90e7044a6f5b192affd33e613ed75106
Vasanth Kumar C <vasanth49468@gmail.com>
2025-10-04 10:26:13 +0530

    Build Error Fix 2

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-import dynamic from 'next/dynamic'
-
-const ChatInputBox = dynamic(() => import('./_components/ChatInputBox'), { ssr: false });
+"use client"
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
+  const {setTheme} = useTheme();
   return (
     <div>
       <ChatInputBox />
     </div>
   );
 }
+
+

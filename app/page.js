46496b785a62a402352ca3956abc619d367a3972
Vasanth Kumar C <vasanth49468@gmail.com>
2025-10-04 10:53:48 +0530

    Build Error Fix 5

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
-import dynamic from 'next/dynamic'
-import React, { Suspense } from 'react'
-const ChatInputBox = dynamic(() => import('./_components/ChatInputBox'), { ssr: false });
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
+  const {setTheme} = useTheme();
   return (
     <div>
-      <Suspense fallback={<div />}>
-        <ChatInputBox />
-      </Suspense>
+      <ChatInputBox />
     </div>
   );
 }
